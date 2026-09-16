8558117c07c026ce2a3e1c583ebce3d780713ae5
Matheus Henrique <matheus260524@gmail.com>
2026-09-16 11:31:39 -0300

    Add files via upload

---
+++ desafio/MATHEUS_CURSO_FULLTIME/DESAFIO_lista/logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+const listagenerica = []
+
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+
+nomes = ['banana', 'maca', 'pera'];
+console.log(nomes[0]);
+console.log(nomes[1]);
+console.log(nomes [2]);
+
